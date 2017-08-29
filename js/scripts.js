7b4fab196ea1625f0d14b4044710ff297e1ed4c4
corey redding <coreysnightout@gmail.com>
2017-08-29 15:54:46 -0700

    add products to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+  $(".container-bars").hide();
 
+  $(".nav-logo").click(function() {
+    $(".container-bars").slideToggle("slow", function(){
 
-)}
+    });
+  });
+});
