6502634df9c62e751807a38a69077f2afd6cd700
corey redding <coreysnightout@gmail.com>
2017-08-29 18:44:45 -0700

    add styling changes and javascript to dropdown menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
 
-  $(".container-bars").hide();
+  // $(".container-bars").hide();
+  //
+  // $(".nav-logo").click(function() {
+  //   $(".container-bars").slideToggle("slow", function(){
+  //
+  //   });
+  // });
 
-  $(".nav-logo").click(function() {
-    $(".container-bars").slideToggle("slow", function(){
+  $(".dropdown").hide();
 
-    });
+  $(".container-bars").click(function() {
+    $(".dropdown").slideToggle();
+    // $(".container-bars").hide();
+  });
+
+  $(".dropdown").click(function() {
+    // $(".container-bars").slideToggle();
+    $(".dropdown").hide();
   });
 });
