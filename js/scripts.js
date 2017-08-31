7a7e163f5cfc193ba6eadb2c91912900f5cc2907
corey redding <coreysnightout@gmail.com>
2017-08-31 13:28:43 -0700

    add footer area, modal for adding products to cart and add to cart button on products

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,30 @@ $(document).ready(function() {
 
   $(".container-bars").click(function() {
     $(".dropdown").fadeToggle();
-    // $(".container-bars").hide();
   });
 
   $(".dropdown").click(function() {
-    // $(".container-bars").slideToggle();
     $(".dropdown").fade();
   });
+
+  $(".nav-link3").click(function(){
+    $("body, html").stop()
+    $("body, html").animate({ scrollTop: $("#about-area").offset().top},"slow");
+  })
+
+  $(".foot-pic1").click(function(){
+    $("body, html").stop()
+    $("body, html").animate({ scrollTop: $(".nav-logo").offset().top},"slow");
+  })
+
+  $(".btn").click(function() {
+    $("#modal").css("display", "block");
+    $(".mask-off").css("display", "block");
+  })
+  $("#modal-btn").click(function() {
+    $("#modal").css("display", "none");
+    $(".mask-off").css("display", "none");
+  })
 
 });
 
